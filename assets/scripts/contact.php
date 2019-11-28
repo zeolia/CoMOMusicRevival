2b167640381deb11df77c81ab4cbc979b5fb3ee1
griffinstiens <griffinstiens@gmail.com>
2019-11-28 11:12:02 -0600

    test

--- assets/scripts/contact.php
+++ assets/scripts/contact.php
@@ -1,13 +1,12 @@
 <?php
 
     if (isset($_POST['submit'])) {
+
         $firstName = $_POST['first_name'];
         $lastName = $_POST['last_name'];
         $bandName = $_POST['band_name'];
         $email = $_POST['email'];
         $details = $_POST['textarea'];
-
-
 
         $mailTo = 'comomusic.project@gmail.com';
         $headers = "From: ".$email;
@@ -16,7 +15,7 @@
 
         mail($mailTo, $bandName, $text, $headers);
 
-        header("Location: ../../pages/contact.component.php?messageSuccess");
+        header("Location: ../../pages/contact/contact.component.php?messageSuccess");
     }
 
 
